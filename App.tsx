749b5886bcdb3c8e3010b4948e59853d981da601
Ayush Chugh <ayushchugh2006@gmail.com>
2022-07-13 22:03:42 +0530

    added basic packages and github issue templates

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import { FC } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
+const App: FC = () => {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
+			<Text>Hello</Text>
 			<StatusBar style='auto' />
 		</View>
 	);
