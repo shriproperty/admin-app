096de0b8e0cfbf34b39006214970f36858b06c26
Ayush Chugh <ayushchugh2006@gmail.com>
2022-07-17 19:40:20 +0530

    update contact radio button on press working now

--- screens/contacts/contact/modals/UpdateContactModal.tsx
+++ screens/contacts/contact/modals/UpdateContactModal.tsx
@@ -25,9 +25,11 @@ interface UpdateContactModalProps {
  * @return {JSX.Element} JSX.Element
  */
 const UpdateContactModal: FC<UpdateContactModalProps> = props => {
-	const [radioValue, setRadioValue] = useState<
-		'Pending' | 'In Progress' | 'Completed'
-	>('Pending');
+	const [radioValue, setRadioValue] = useState<Contact['status']>('Pending');
+
+	const updateRadioValue = (value: Contact['status']) => {
+		setRadioValue(value);
+	};
 
 	return (
 		<Modal
@@ -39,36 +41,39 @@ const UpdateContactModal: FC<UpdateContactModalProps> = props => {
 		>
 			<Pressable
 				style={styles.modalRadioButtonContainer}
-				onPress={() => setRadioValue('Pending')}
+				onPress={updateRadioValue.bind(this, 'Pending')}
 			>
 				<RadioButton
 					value='Pending'
 					color={Colors.primary}
 					status={radioValue === 'Pending' ? 'checked' : 'unchecked'}
+					onPress={updateRadioValue.bind(this, 'Pending')}
 				/>
 				<Text style={styles.modalRadioButtonText}>Pending</Text>
 			</Pressable>
 
 			<Pressable
 				style={styles.modalRadioButtonContainer}
-				onPress={() => setRadioValue('In Progress')}
+				onPress={updateRadioValue.bind(this, 'In Progress')}
 			>
 				<RadioButton
 					value='In Progress'
 					color={Colors.primary}
 					status={radioValue === 'In Progress' ? 'checked' : 'unchecked'}
+					onPress={updateRadioValue.bind(this, 'In Progress')}
 				/>
 				<Text style={styles.modalRadioButtonText}>In Progress</Text>
 			</Pressable>
 
 			<Pressable
 				style={styles.modalRadioButtonContainer}
-				onPress={() => setRadioValue('Completed')}
+				onPress={updateRadioValue.bind(this, 'Completed')}
 			>
 				<RadioButton
 					value='Completed'
 					color={Colors.primary}
 					status={radioValue === 'Completed' ? 'checked' : 'unchecked'}
+					onPress={updateRadioValue.bind(this, 'Completed')}
 				/>
 				<Text style={styles.modalRadioButtonText}>Completed</Text>
 			</Pressable>
